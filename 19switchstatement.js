447918e5b29cec6384e26e0236ef1b3cbd5db44b
sabahkhan1 <130240662+sabahkhan1@users.noreply.github.com>
2023-04-09 11:43:53 -0700

    Add files via upload

---
+++ 19switchstatement.js
@@ -0,0 +1,47 @@
+// switch statement
+
+let day = 4;
+
+// if(day === 0) {
+//     console.log("Sunday");
+// } else if(day === 1) {
+//     console.log("Monday");
+// } else if(day === 2) {
+//     console.log("Tuesday");
+// } else if(day === 3) {
+//     console.log("Wednesday");
+// } else if(day === 4) {
+//     console.log("Thursday");
+// } else if(day === 5) {
+//     console.log("Friday");
+// } else if(day === 7) {
+//     console.log("Saturday");
+// } else {
+//     console.log("Invalid Day");
+// }
+
+switch(day) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Satday");
+        break;
+        default:
+            console.log("Invalid Day");
+}
